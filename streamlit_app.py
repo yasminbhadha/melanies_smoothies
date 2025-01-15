2ffa085ec38606ac2e718e3e526297a164082472
yasminbhadha <162388338+yasminbhadha@users.noreply.github.com>
2025-01-16 09:27:34 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,15 @@ if ingredients_list:
 # New section to display the smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
+
+
+
+
+
+
 
 
